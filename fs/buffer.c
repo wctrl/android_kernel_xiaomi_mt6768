d069fb0032c84183d630b7f07e80489431fe9bd0
Jens Axboe <axboe@kernel.dk>
2017-09-27 05:45:36 -0600

    buffer: grow_dev_page() should use __GFP_NOFAIL for all cases
    We currently use it for find_or_create_page(), which means that it
    cannot fail. Ensure we also pass in 'retry == true' to
    alloc_page_buffers(), which also ensure that it cannot fail.
    
    After this, there are no failure cases in grow_dev_page() that
    occur because of a failed memory allocation.
    
    Reviewed-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: ukashah1 <ukashah60@gmail.com>

--- fs/buffer.c
+++ fs/buffer.c
@@ -1023,7 +1023,7 @@ grow_dev_page(struct block_device *bdev, sector_t block,
 	/*
 	 * Allocate some buffers for this page
 	 */
-	bh = alloc_page_buffers(page, size, false);
+	bh = alloc_page_buffers(page, size, true);
 
 	/*
 	 * Link the page to the buffers and initialise them.  Take the
